f67b757c4d56a1ff4e4d314f408d494b8a2c2002
Henzo <henzo.2021010272@aluno.iffar.edu.br>
2024-12-03 23:45:08 -0300

    Teste

---
+++ js/principal.js
@@ -0,0 +1,8 @@
+var banners = ["Do lixo ao luxo!","Reaproveitar é aproveitar!"];
+var banner = 0;
+
+function trocaBanner() {
+  banner = (banner + 1) % 2;
+  document.querySelector("h2#mensagem").textContent = banners[banner];
+}
+setInterval(trocaBanner, 1000);
